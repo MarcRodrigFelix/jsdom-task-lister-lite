82079553a131681c9b3dea71cc12e5f96680de23
Marcos Rodriguez <m.rodrig.felix@gmail.com>
2021-01-30 19:09:52 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -7,12 +7,21 @@ document.addEventListener("DOMContentLoaded", () => {
     event.preventDefault()
     const task = document.getElementById('new-task-description')
     const li = document.createElement('li')
+    let btn = document.createElement('BUTTON')
+    btn.innerHTML = "X"
 
     ul.appendChild(li)
     li.innerHTML = task.value
     task.value = "" // reset input value
-  })
-  
+    li.appendChild(btn)
 
+    btn.style.margin = '5px'
+    btn.style.fontSize = '10px'
+    btn.style.cursor = 'pointer'
+
+    btn.addEventListener('click', () => {
+      li.remove()
+    })
+  })
   
 });
