7b3fcc61aa34a3cab1268697a3e3262cf8ae379a
Marcos Rodriguez <m.rodrig.felix@gmail.com>
2021-01-30 17:54:14 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const myForm = document.querySelector('form')
+  const ul = document.querySelector('#tasks')
+
+
+  myForm.addEventListener('submit', event => {
+    event.preventDefault()
+    const task = document.getElementById('new-task-description')
+    const li = document.createElement('li')
+
+    ul.appendChild(li)
+    li.innerHTML = task.value
+    task.value = "" // reset input value
+  })
+  
+
+  
 });
